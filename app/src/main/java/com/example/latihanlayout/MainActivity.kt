ec3514f3d336059a7c71e0a1ab5426a91e205248
Jonathan K <jkamagi41@gmail.com>
2025-03-31 17:23:02 +0800

    first commit

---
+++ app/src/main/java/com/example/latihanlayout/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.latihanlayout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var inputUsername: TextInputEditText = findViewById(R.id.input_username)
+        var inputPassword: TextInputEditText = findViewById(R.id.input_password)
+        var tombolLogin: Button = findViewById(R.id.button_login)
+
+        tombolLogin.setOnClickListener {
+            Toast.makeText(this, inputUsername.text.toString(), Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+}
